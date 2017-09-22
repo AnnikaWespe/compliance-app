b623dd8fb959061889014ca799a806a24249a001
Annika Westphäling <westphaeling@mobile-software.ag>
2017-09-22 13:00:22 +0200

    Testversion

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,27 +1,38 @@
-import {Component} from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, Renderer2} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {QuestionScreenComponent} from '../getDonation/questionScreen/questionScreen.component';
 import {DecisionTreeService} from '../../services/decisionTree/decisionTreeData.service';
 import {GlossaryService} from '../../services/glossary/glossary.service';
 import {TranslateService} from '@ngx-translate/core';
-import {DomSanitizer} from '@angular/platform-browser';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.component.html'
 })
-export class HomePageComponent {
+export class HomePageComponent implements AfterViewChecked {
 
   decisionTreeData;
-  translatedText;
+  title: SafeHtml;
+  info: SafeHtml;
+
+  clickHandlersAdded: boolean;
 
   constructor(public navCtrl: NavController,
               decisionTreeService: DecisionTreeService,
               private glossaryService: GlossaryService,
               private translateService: TranslateService,
+              private elRef: ElementRef,
+              private renderer: Renderer2,
               private domSanitizer: DomSanitizer) {
     this.decisionTreeData = decisionTreeService.getDecisionTreeData();
     this.getTranslation();
+  }
+
+  ngAfterViewChecked() {
+    if (!this.clickHandlersAdded) {
+      this.clickHandlersAdded = this.glossaryService.injectClickEventHandler(this.elRef, this.renderer);
+    }
   }
 
   startProcess(option) {
@@ -33,9 +44,15 @@ export class HomePageComponent {
   }
 
   getTranslation() {
-    this.translateService.get('test-div').subscribe(
+    this.translateService.get('home.title').subscribe(
       (value) => {
-        this.translatedText = this.domSanitizer.bypassSecurityTrustHtml(value);
+        this.title = this.domSanitizer.bypassSecurityTrustHtml(value);
+      }
+    );
+    this.translateService.get('home.info').subscribe(
+      (value) => {
+        let stringWithSpanTags = this.glossaryService.injectSpanTags(value);
+        this.info = this.domSanitizer.bypassSecurityTrustHtml(stringWithSpanTags);
       }
     );
   }
--- src/services/glossary/glossary.service.ts
+++ src/services/glossary/glossary.service.ts
@@ -1,25 +1,45 @@
-import {Injectable} from '@angular/core';
+import {ElementRef, Injectable, Renderer2} from '@angular/core';
 import {GLOSSARY} from './glossary.data';
 import {AlertController} from 'ionic-angular';
-import {TranslateService} from '@ngx-translate/core';
 
 
 @Injectable()
 export class GlossaryService {
-  constructor(public alertCtrl: AlertController,
-              private translateService: TranslateService) {
+  glossary = GLOSSARY;
+
+  constructor(public alertCtrl: AlertController) {
   }
 
   createPopUp(term) {
-    this.translateService.get(term).subscribe(
-      value => {
-        let glossary = GLOSSARY;
-        let alert = this.alertCtrl.create({
-          title: value,
-          message: glossary[value],
-        });
-        alert.present();
-      }
-    );
+    let alert = this.alertCtrl.create({
+      title: term,
+      message: this.glossary[term],
+    });
+    alert.present();
   }
+
+  injectClickEventHandler(elRef: ElementRef, renderer: Renderer2) {
+    let foundElements = elRef.nativeElement.querySelectorAll('.open-info');
+    if (foundElements.length) {
+      foundElements.forEach((element) => {
+        element.setAttribute('tappable', '');
+        renderer.listen(element, 'click', () => {
+          let glossaryTerm = element.getAttribute('glossary');
+          this.createPopUp(glossaryTerm);
+        });
+      });
+      return true;
+    }
+    return false;
+  }
+
+  injectSpanTags(string){
+    let regExp = /\[([^)]+)\]/;
+    let matches = regExp.exec(string);
+    let stringToBeReplaced = matches[0];
+    let content = matches[1].split(',');
+    console.log(stringToBeReplaced, content);
+    return string;
+  }
+
 }
