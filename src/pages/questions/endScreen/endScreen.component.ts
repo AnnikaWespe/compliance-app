1107b975a86abac31ff17eb924b94b994c709c03
Annika Westphäling <westphaeling@mobile-software.ag>
2017-10-12 16:49:16 +0200

    End Screen mit Übersetzung

--- src/pages/questions/endScreen/endScreen.component.ts
+++ src/pages/questions/endScreen/endScreen.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {HomePageComponent} from '../../home/home.component';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'page-end-screen',
@@ -9,12 +10,25 @@ import {HomePageComponent} from '../../home/home.component';
 export class EndScreenComponent {
 
   process;
+  documentationRequired;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private translateService: TranslateService) {
     this.process = navParams.get('process');
+    this.getTranslation();
   }
 
   goToStartPage(){
     this.navCtrl.setRoot(HomePageComponent);
+  }
+
+  getTranslation() {
+    this.translateService.get('secondary-pages.endScreen.text_documentation').subscribe(
+      value => {
+        this.documentationRequired = value;
+        console.log(this.documentationRequired);
+      }
+    );
   }
 }
